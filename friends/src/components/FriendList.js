756bfac0a831e6d1e8ca99dd68db9824e5c05a83
Daniel <danstad2012@gmail.com>
2019-11-12 16:29:32 -0800

    display friendslist

--- friends/src/components/FriendList.js
+++ friends/src/components/FriendList.js
@@ -10,6 +10,7 @@ const axiosWithAuth = () => {
 };
 
 const FriendList = () => {
+  const [friends, setFriends] = useState([])
   useEffect(() => {
     getData();
     if(!sessionStorage.getItem('token')){
@@ -17,19 +18,30 @@ const FriendList = () => {
     } else {
       console.info('we are logged in')
     }
-  })
+  },[])
 
   const getData = () => {
     axios.get(`http://localhost:5000/api/friends`,{
       headers: {authorization: sessionStorage.getItem('token')}
     })
     .then(res =>{
-      const friends = res.data
-      console.log(friends)
+      setFriends(res.data)
+      
     })
   }
+  console.log(friends)
+
   return (
-    <div> Friends</div>
+    <div> 
+      <h2>Friends:</h2>
+      <ul>
+        {friends.map(f => (
+          <li key={f.id}>
+              {f.name} is {f.age} years old. Reach them at {f.email}
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
